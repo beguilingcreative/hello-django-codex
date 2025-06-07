e1b64e9c1f8aecede305dc853c1bd851549537c2
beguilingcreative <144760718+beguilingcreative@users.noreply.github.com>
2025-06-07 17:14:59 +0800

    docs: clarify database usage

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -39,7 +39,8 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.dummy',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
