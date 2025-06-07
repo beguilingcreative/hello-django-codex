4ce978fc3f3a282775e1d6944cbee24d2529750e
beguilingcreative <144760718+beguilingcreative@users.noreply.github.com>
2025-06-07 16:36:50 +0800

    Add message board with Bootstrap UI

---
+++ hello/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Message
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title', 'content')
